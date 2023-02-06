dfe7598d5f448d614b068c5de20d38a62f5dbc2f
jeonghun lee <jeonghunlee@dxm.local>
2023-02-06 10:29:57 +0900

    auth button complete

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
+  final FaIcon icon;
   final String text;
 
-  const AuthButton({super.key, required this.text});
+  const AuthButton({super.key, required this.text, required this.icon});
 
   @override
   Widget build(BuildContext context) {
@@ -12,24 +14,33 @@ class AuthButton extends StatelessWidget {
       //부모사이즈에 맞춰서 크기 조절
       widthFactor: 1,
       child: Container(
-        margin: const EdgeInsets.symmetric(
-          vertical: Sizes.size10,
-        ),
-        padding: const EdgeInsets.symmetric(
-          vertical: Sizes.size14,
+        padding: const EdgeInsets.all(
+          Sizes.size14,
         ),
         decoration: BoxDecoration(
             border: Border.all(
           color: Colors.grey.shade300,
           width: Sizes.size1,
         )),
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          // 위로 쌓게 해주는 위젯
+          alignment: Alignment.center,
+          children: [
+            Align(
+              //Stack에 있는 위젯 하나의 정렬만 변경할 수 있게 해줌
+              alignment: Alignment.centerLeft,
+              //Stack에 있는 위젯 하나의 정렬만 변경할 수 있게 해줌
+              child: icon,
+            ),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
         ),
       ),
     );
